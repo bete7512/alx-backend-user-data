f0de7d5d1e6f8dc8e812baa46a2d0cd0e44b129a
bete7512 <betekbebe@gmail.com>
2023-02-08 19:44:31 +0300

    messages for that

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
+@app.errorhandler(401)
 def not_found(error) -> str:
     """ Not found handler
     """
