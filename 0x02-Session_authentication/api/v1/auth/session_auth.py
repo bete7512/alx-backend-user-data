8f15826344432f8f5f7a5160615d871aa8233742
bete7512 <betekbebe@gmail.com>
2023-02-09 16:12:09 +0300

    special zero

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,3 +9,12 @@ from models.user import User
 
 class SessionAuth(Auth):
     """"" SessionAuth class, Require auth with stars """""
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """"" Create session """""
+        if user_id is None or type(user_id) is not str:
+            return None
+        from uuid import uuid4
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
