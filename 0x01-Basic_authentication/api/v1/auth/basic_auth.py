84dcf7fd0f9a933c3852b8816b61286c435dc32d
bete7512 <betekbebe@gmail.com>
2023-02-09 14:00:56 +0300

    for second deadline

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,3 +35,15 @@ class BasicAuth(Auth):
             return decodedValue
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """"" Extract user credentials"""
+        if decoded_base64_authorization_header is None or type(
+                decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user = decoded_base64_authorization_header.split(':')
+        return user[0], user[1]
