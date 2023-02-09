0f0a8d762b7cfbb1a876f0c4ae21b24ef5e7d9fe
bete7512 <betekbebe@gmail.com>
2023-02-09 13:48:46 +0300

    may be the right solution

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,18 +9,24 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth class, Require auth with stars """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
         """"" Extract base64 authorization header"""
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or type(
+                authorization_header) is not str:
             return None
         if authorization_header[:6] != "Basic ":
             return None
         base = authorization_header.split(' ')
         return base[1]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
         """"" Decode base64 authorization header"""
-        if base64_authorization_header is None or type(base64_authorization_header) is not str:
+        if base64_authorization_header is None or type(
+                base64_authorization_header) is not str:
             return None
         try:
             baseEncode = base64_authorization_header.encode('utf-8')
