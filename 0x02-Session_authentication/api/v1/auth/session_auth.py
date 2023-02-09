30d7103b7ce333056e2f2251faa0cdd3fe00f445
bete7512 <betekbebe@gmail.com>
2023-02-09 20:05:29 +0300

    session and cookies

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,10 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+    def current_user(self, request=None):
+        """"" Current user """""
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
