c64ae72ff483485cfa8242de7a8086d93ea6c8b5
bete7512 <betekbebe@gmail.com>
2023-02-09 11:52:20 +0300

    special Thing

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Auth class, Require auth with stars """
 from flask import request
+from base64 import b64decode
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
@@ -15,3 +16,14 @@ class BasicAuth(Auth):
             return None
         base = authorization_header.split(' ')
         return base[1]
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """"" Decode base64 authorization header"""
+        if base64_authorization_header is None or type(base64_authorization_header) is not str:
+            return None
+        try:
+            baseEncode = base64_authorization_header.encode('utf-8')
+            baseDecode = b64decode(baseEncode)
+            decodedValue = baseDecode.decode('utf-8')
+            return decodedValue
+        except Exception:
+            return None
