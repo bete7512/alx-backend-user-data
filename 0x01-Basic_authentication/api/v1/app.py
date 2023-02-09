9921649668c7d4e8565e0f26dd93b3173fa47100
bete7512 <betekbebe@gmail.com>
2023-02-09 11:19:17 +0300

    send fir it

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -24,7 +24,7 @@ def unauthorized(error) -> str:
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ error handler for (forbidden) 403 status code """
-    return jsonify({"error": "Forbidden"}), 403
+    return jsonify({ "error": "Forbidden" }), 403
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -38,4 +38,4 @@ def stats() -> str:
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/forbidden    """
-    abort(401)
+    abort(403)
