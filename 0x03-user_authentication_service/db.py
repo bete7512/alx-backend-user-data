c2473e0e3451db93575e56f223bfee14192cb5a6
bete7512 <betekbebe@gmail.com>
2023-02-17 22:43:34 +0300

    one

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,15 +4,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """ DB class """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
+        """ Initialize a new DB instance """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -20,16 +22,33 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """ Memoized session object """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ save the user to the database and returns a User object """
+        """  save the user to the database and returns a User object """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        print(user)
         return user
+    def find_user_by(self, **kwargs) -> User:
+        """returns the first row found in the users table"""
+        if not kwargs:
+            raise InvalidRequestError
+
+        cols = ["id", "email", "hashed_password", "session_id", "reset_token"]
+
+        for arg in kwargs:
+            if arg not in cols:
+                raise InvalidRequestError
+
+        found = self._session.query(User).filter_by(**kwargs).first()
+
+        if found:
+            return found
+        else:
+            raise 
