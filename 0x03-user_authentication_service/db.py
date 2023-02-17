ca4ff1c9c73cc59f3161bdf625e9fdaf7bec50b2
bete7512 <betekbebe@gmail.com>
2023-02-17 21:29:23 +0300

    maker space

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """create new user"""
+        """create user on db"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
